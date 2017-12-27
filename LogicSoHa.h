ebf70ba45d2bbbca05e1912232b5679777a473d7
pinorr <505971450@qq.com>
2017-12-27 15:28:04 +0800

    Update LogicSoHa.h

--- LogicSoHa.h
+++ LogicSoHa.h
@@ -6,15 +6,16 @@
 #include <algorithm>
 #include <wtypes.h>
 #include <map>
+#include <unordered_map>
 
 //////////////////////////////////////////////////////////////////////////
+//#define		USE_MAP
 
 #define		MAX_CARD_NUM			28									//单副牌数目
 
 //数值掩码
 #define		MASK_COLOR				0xF0								//花色掩码
 #define		MASK_VALUE				0x0F								//数值掩码
-
 enum enCor
 {
 	enCor_block	= 0,				//方块
@@ -38,6 +39,14 @@ enum enType
 	enType_max,
 };
 
+struct stData
+{
+	DWORD	dwVal;
+	BYTE	byMaxCor;				//索引
+
+	stData() { memset(this, 0, sizeof(*this)); }
+};
+
 //////////////////////////////////////////////////////////////////////////
 
 //游戏逻辑类
@@ -45,8 +54,9 @@ class CGameLogicPino
 {
 	//变量定义
 protected:	
-	std::map<BYTE, BYTE>			m_mapIndexByVal;
-	std::map<DWORD, DWORD>			m_mapKeyData;
+	std::unordered_map<BYTE, BYTE>				m_mapIndexByVal;
+	std::unordered_map<DWORD, DWORD>			m_mapKeyData;
+	stData										m_KeyData[16384];
 
 	//函数定义
 public:
@@ -61,18 +71,18 @@ public:
 public:
 	//由牌组得到key
 	DWORD getKeyByCards(const BYTE cbData[], BYTE num);
-  //由key得到牌组
+	//由key得到牌组
 	void  getCardsByKey(DWORD dwKey, BYTE cbData[5]);
-	//获取牌值
-	BYTE  GetCardVal(BYTE cbData);
-	//获取牌颜色
-	BYTE  GetCardCor(BYTE cbData);
 	//获取牌组权值
 	DWORD getLogicVal(const BYTE cbData[], BYTE num);
+	//获取牌组权值
+	DWORD getLogicVal2(const BYTE cbData[], BYTE num);
 	//获取牌组类型
-	BYTE  GetCardType(const BYTE cbData[], BYTE num);
-	//对比扑克
-	bool  CompareCard(const BYTE cbData1[], const BYTE cbData2[], BYTE num1, BYTE num2);	
+	BYTE  getCardsType(const BYTE cbData[], BYTE num);
+	//获取获胜机率
+	float get1WinPer(const BYTE cbData1[], const BYTE cbData2[], BYTE num1, BYTE num2);
+private:
+	DWORD getMapInfo(std::unordered_map<DWORD, DWORD> &mapUnOrder, BYTE byTemp[5], BYTE num, const bool bFlag[MAX_CARD_NUM]);
 };
 
 //////////////////////////////////////////////////////////////////////////
